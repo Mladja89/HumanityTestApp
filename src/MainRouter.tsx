ed0f17d6ddbe0894042720334b2ad485d3e6fd2f
Mladja89 <goamladja2006@gmail.com>
2020-09-21 08:12:58 +0200

    fixed default route

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { Schedule } from './pages/Schedule'
 import { ShiftDetails } from './pages/ShiftDetails'
 import * as routes from './routes'
@@ -8,6 +8,7 @@ export function MainRouter() {
   return (
     <BrowserRouter>
       <Switch>
+        <Redirect exact from="/" to={routes.defaultSchedule} />
         <Route path={routes.defaultSchedule} exact component={Schedule} />
         <Route path={routes.schedule(':date')} component={Schedule} />
         <Route path={routes.shift(':id')} component={ShiftDetails} />
